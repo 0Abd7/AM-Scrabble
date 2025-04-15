7842e4e6c9ccf1c9076324bdac1251c553240042
0Abd7 <abdououari@icloud.com>
2025-04-15 05:34:30 +0200

    Update gaddag.hpp

--- src/gaddag.hpp
+++ src/gaddag.hpp
@@ -1,20 +1,30 @@
-#include <iostream>
-#include <unordered_map>
-#include <string>
-#include <vector>
+#pragma once
 
-using namespace std;
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <stack>
+#include <iostream>
+#include "board.hpp"
+#include "joueur.hpp"
+#include "spot.hpp"
+#include "direction.hpp"
+#include "gaddagNoeud.hpp"
+#include <unordered_set>
+#include <set>
 
 class Gaddag {
-
-
-    struct Noeud{
-        bool motTermine = false;
-        array<Noeud*, 26> voisins = {};
-    };
-
+public:
+    GaddagNode *racine;
     Gaddag();
-    Noeud* racine;
-    void insertNoeud(Noeud* noeud, const string& mot, size_t index);
-    inserer
-}
+    std::unordered_set<std::string> dictionnaire_complet;
+    
+    void ajouter_mot(const std::string& mot);
+    bool cherche_mot_sans_plus(const std::string &mot) const;
+    void dictionnaire();
+    void genereCombinaisons(const std::string &lettres, std::string actuelle, std::set<std::string> &combinations);
+    std::vector<std::string> trouveMotValideChevalet(const Gaddag &gaddag, const std::string &lettresChevalet);
+    bool checkMotsCroises(const Board &board, int ligne, int col, bool horizontal, const std::string &mot);
+    std::string getMotHorizontalComplet(const Board &board, int ligne, int col) const;
+    std::string getMotVerticalComplet(const Board &board, int ligne, int col) const;
+};
